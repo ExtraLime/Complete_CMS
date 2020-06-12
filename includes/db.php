e362c9b61f82d43f67092ba8fee41977f2dd9d3b
ExtraLime <willdox7@live.com>
2020-06-12 00:14:06 -0700

    more cleardb config

--- includes/db.php
+++ includes/db.php
@@ -1,6 +1,5 @@
 <?php 
 
-
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 $server = $url["host"];
@@ -8,10 +7,7 @@ $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
 
-$connection = new mysqli($server, $username, $password, $db);
-
-
-
+$conn = new mysqli($server, $username, $password, $db);
 if($connection){
    echo "We are Connected";
 }else {
