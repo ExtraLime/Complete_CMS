ac71068e7292e6897c00eb51e5a22f8612f5e470
ExtraLime <willdox7@live.com>
2020-07-09 16:16:32 -0700

    fixes

--- testup.php
+++ testup.php
@@ -1,13 +1,16 @@
 <?php
 require('vendor/autoload.php');
 // this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
+
 $s3 = new Aws\S3\S3Client([
+    'accessKeyId'=> getenv(BUCKETEER_AWS_ACCESS_KEY_ID),
+    'secretAccessKey'=> getenv(BUCKETEER_AWS_SECRET_ACCESS_KEY),
     'version'  => '2006-03-01',
     'region'   => 'us-east-1',
 ]);
 $bucket = getenv('BUCKETEER_BUCKET_NAME')?: die('No "S3_BUCKET" config var in found in env!');
 ?>
-<?php print_r($bucket); echo $s3;?>
+<?php print_r($bucket);?>
 <html>
     <head><meta charset="UTF-8"></head>
     <body>
